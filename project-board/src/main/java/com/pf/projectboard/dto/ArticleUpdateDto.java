9a091ee6f04735f6c43cd84d50d09c98cd9428bf
JIHYEON <wlgus120332@gmail.com>
2022-07-15 21:22:18 +0900

    #19 - 게시글 등록 및 수정을 위한 DTO 추가
    게시글 등록을 위한 ArticleDto와 게시글 수정을 위한 ArticleUpdateDto를 record타입으로 추가

---
+++ project-board/src/main/java/com/pf/projectboard/dto/ArticleUpdateDto.java
@@ -0,0 +1,12 @@
+package com.pf.projectboard.dto;
+
+public record ArticleUpdateDto(
+        String title,
+        String content,
+        String hashtag
+) {
+
+    public static ArticleUpdateDto of (String title, String content, String hashtag) {
+        return new ArticleUpdateDto(title, content, hashtag);
+    }
+}
